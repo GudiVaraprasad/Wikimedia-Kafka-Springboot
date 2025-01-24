8046e401873e32a7b175e64ed5d7040858c724ca
GudiVaraprasad <gudi.varaprasad@gmail.com>
2025-01-23 19:28:15 -0500

    code base-consumer

---
+++ wikimedia-consumer/src/main/java/consumer/db/SBConsumerApp.java
@@ -0,0 +1,13 @@
+package consumer.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBConsumerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SBConsumerApp.class);
+    }
+}
